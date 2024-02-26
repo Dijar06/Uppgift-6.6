fe2ec7ce55787b58eaee73778a73ef62719815f0
Dijar06 <dize06001@utb.vaxjo.se>
2024-02-26 14:15:54 +0100

    Add project files.

---
+++ Uppgift 6.6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in en mening:");
+            string mening = Console.ReadLine();
+            Metod(mening);
+        }
+
+        static void Metod(string mening)
+        {
+            string[] ord = mening.Split(' ');
+
+            for (int i = 0; i < ord.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(ord[i].ToUpper() + " ");
+                }
+                else
+                {
+                    Console.Write(ord[i].ToLower() + " ");
+                }
+            }
+        }
+    }
+}
